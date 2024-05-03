d372d7825816dafa7b12961dc8a73933e074fc83
Adam Boczek <adam.boczek@nttdata.com>
2024-05-03 17:29:49 +0100

    add new modules for groups of various scenarios

---
+++ c8-demo-scenarios/c8-demo-scenarios-group1/src/main/java/com/boczek/c8demo/scenarios/group1/ScenariosGroup1Application.java
@@ -0,0 +1,12 @@
+package com.boczek.c8demo.scenarios.group1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScenariosGroup1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScenariosGroup1Application.class, args);
+	}
+}
